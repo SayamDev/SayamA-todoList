9aaa4bcfc195e0946faf0febce7ba84727181280
Sayam Ajmal <itssanesayam@gmail.com>
2022-04-19 13:13:12 +0100

    added handlesubmit which handle todo an items list

--- src/App.js
+++ src/App.js
@@ -13,6 +13,24 @@ function Todo ({todo}) {
 //creating to do items
 function TodoForm({addTodo}) {
   const [value, setValue] = useState("")
+
+  const handleSubmit = e =>{
+    e.preventDefault() //stops refreshing 
+    if (!value) return
+    addTodo(value)
+    setValue("")
+  }
+
+  return (
+    <form onSubmit = {handleSubmit}>
+      <input 
+      type="text"
+      className= "input"
+      value={value}
+      onChange = {e => setValue (e.target.value)}
+      />
+    </form>
+  )
 }
 
 //todos = name of my state //setTodos what you are going to use to set the state
