4e21517c88bce4d1718ab75a5847ed32a277d332
Sayam Ajmal <itssanesayam@gmail.com>
2022-04-19 13:04:57 +0100

    created a new function to create items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function Todo ({todo}) {
   return (
-    <div className="App">
-     
+    <div className='todo'>
+      {todo.text}
+    </div>
+  )
+}
+
+
+//creating to do items
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState("")
+}
+
+//todos = name of my state //setTodos what you are going to use to set the state
+
+
+
+function App() {
+  const [todos, setTodos] = useState([ 
+    {text: "learn more about react"},
+    {text: "gucci is life ok"},
+    {text: "learn about okay life"},
+  ])
+
+
+  return (
+    <div className='app'>
+      <div className='todo-list'>
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            />
+        ))}
+      </div> 
     </div>
   );
 }
