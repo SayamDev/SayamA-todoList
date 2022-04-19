900211e1c40ae24ae41357a902910a3a6cb17650
Sayam Ajmal <itssanesayam@gmail.com>
2022-04-19 13:49:12 +0100

    added done button

--- src/App.js
+++ src/App.js
@@ -1,90 +1,92 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function Todo ({todo, index, completeTodo}) {
+
+// using completeTodo in the Todo function,
+function Todo({ todo, index, completeTodo }) {
   return (
-    <div className='todo'
-    style={{textDecoration: todo.isCompleted ? "line-through" : ""}}
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
     >
       {todo.text}
+      <div>
+      <button onClick = {() => completeTodo (index)}>Done </button>
+      </div>
     </div>
-  )
+  );
 }
 
-
 //creating to do items
-function TodoForm({addTodo}) {
-  const [value, setValue] = useState("")
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
 
-  const handleSubmit = e =>{
-    e.preventDefault() //stops refreshing 
-    if (!value) return
-    addTodo(value)
-    setValue("")
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault(); //stops refreshing
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
 
   return (
-    <form onSubmit = {handleSubmit}>
-      <input 
-      type="text"
-      className= "input"
-      value={value}
-      onChange = {e => setValue (e.target.value)}
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
       />
     </form>
-  )
+  );
 }
 
 //todos = name of my state //setTodos what you are going to use to set the state
 
-
 //adding in an isCompleted: false value, you will set that to false to begin with and will, when prompted, change that to true.
 function App() {
-  const [todos, setTodos] = useState([ 
+  const [todos, setTodos] = useState([
     {
       text: "learn react hoola",
-      isCompleted: false
+      isCompleted: false,
     },
     {
       text: "hello lola whats up",
-      isCompleted: false
+      isCompleted: false,
     },
     {
       text: "gucci is life gucci",
-      isCompleted: false
-    }
-
-  ])
+      isCompleted: false,
+    },
+  ]);
 
   //marking items as complete
   //using the spread operator to grab the current list of items. In this function, I will change the isCompleted status to true so that it knows it is complete. It will then update the state and set the state to the newTodos.
 
-  const completeTodo = index => {
-    const newTodos = [...todos]
-    newTodos[index].isCompleted = true
-    setTodos(newTodos)
-  }
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
 
   //function will be able to grab the existing list of items, add on the new item, and display that new list.
-  const addTodo = text => {
-    const newTodos = [...todos, {text}]
-    setTodos(newTodos)
-  }
-
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
 
   return (
-    <div className='app'>
-      <div className='todo-list'>
+    <div className="app">
+      <div className="todo-list">
         {todos.map((todo, index) => (
           <Todo
             key={index}
             index={index}
             todo={todo}
             completeTodo={completeTodo}
-            />
+          />
         ))}
         <TodoForm addTodo={addTodo} />
-      </div> 
+      </div>
     </div>
   );
 }
